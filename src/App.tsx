9e64f7bca360d7a5350eb994a94fc74fd9d77c87
Kok Kek <kokkekpek@gmail.com>
2023-04-04 15:38:56 +0300

    feat: SCSS and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import './App.scss'
+import logo from './logo.svg'
+
 export const App = (): JSX.Element => (
-  <div className="app">
+  <div className='app'>
     <h1>Hello World!</h1>
     <p>Foo to the bar</p>
+    <img src={logo} height='480' />
   </div>
 )
