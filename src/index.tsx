60f03f1192921fae58ca166434aa8737e53a0f79
Kok Kek <kokkekpek@gmail.com>
2023-04-04 15:53:53 +0300

    feat: modern-normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import 'modern-normalize/modern-normalize.css'
 
 const container = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(container)
