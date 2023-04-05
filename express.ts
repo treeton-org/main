7e1df17d1751ea1cda5fe2f3576337f869af0b31
Kok Kek <kokkekpek@gmail.com>
2023-04-05 18:06:35 +0300

    feat: Port for local `yarn start` and `yarn prod`

--- express.ts
+++ express.ts
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
 import express from 'express'
 const app = express()
-const portNumber = 3000
 const sourceDir = 'dist'
 
+dotenv.config()
+const port = process.env.PORT ?? 3000
+
 app.use(express.static(sourceDir))
-app.listen(portNumber, () => {
-  console.log(`Web server started: http://localhost:${portNumber}`)
+app.listen(port, () => {
+  console.log(`Web server started: http://localhost:${port}`)
 })
--- webpack/dev.ts
+++ webpack/dev.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
 import {merge} from 'webpack-merge'
 import {common} from './common'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import path from 'path'
+
+dotenv.config()
+const port = process.env.PORT ?? 3000
 
 module.exports = merge<Object>(common, {
     mode: 'development',
@@ -10,7 +14,8 @@ module.exports = merge<Object>(common, {
         historyApiFallback: true,
         static: {
             directory: path.join(__dirname, '../public')
-        }
+        },
+        port
     },
     devtool: 'cheap-module-source-map',
     plugins: [new ReactRefreshPlugin()]
