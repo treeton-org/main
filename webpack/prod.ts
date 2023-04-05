ad584945fd469cc080a65c45f85aa648c94f7b8e
Kok Kek <kokkekpek@gmail.com>
2023-04-05 17:59:08 +0300

    feat: Copy files from `public/` to `dist/`

--- webpack/prod.ts
+++ webpack/prod.ts
@@ -1,17 +1,18 @@
 import path from 'path'
 import {merge} from 'webpack-merge'
 import {common} from './common'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 module.exports = merge<Object>(common, {
     mode: 'production',
     output: {
         filename: 'js/bundle.[contenthash].min.js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/',
+        path: path.resolve(__dirname, '../dist')
     },
     devtool: 'source-map',
     externals: {
         'react': 'React',
-        'react-dom': 'ReactDOM',
-    }
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [new CopyWebpackPlugin({patterns: [{from: '../public'}]})]
 })
