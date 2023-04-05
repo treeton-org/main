3d6d600ab159a4593886cf6a1d5dc953ed50307f
Kok Kek <kokkekpek@gmail.com>
2023-04-05 14:36:55 +0300

    feat: SASS modules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
-import './App.sass'
+import css from './App.module.sass'
 import logo from '../assets/logo.svg'
 
 export const App = (): JSX.Element => (
-  <div className='App'>
+  <div className={css.App}>
       <img src={logo} alt='logo' />
-      <div className='name'>Tree TON</div>
-      <div className='soon'>coming soon</div>
+      <div className={css.name}>Tree TON</div>
+      <div className={css.soon}>coming soon</div>
   </div>
 )
